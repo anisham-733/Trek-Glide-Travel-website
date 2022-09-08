89c9d3c3b56b6b39ce8ebd10b9b638d053971412
Anisha Mahajan <82260117+anisham-733@users.noreply.github.com>
2022-09-08 19:04:27 +0530

    Update booking-form.php

--- booking-form.php
+++ booking-form.php
@@ -8,6 +8,11 @@ catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "booking-admin", "pwd" => "Anisham733@123", "Database" => "travel-booking-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:travel-booking-server.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 echo "hello";
 // PHP Data Objects(PDO) Sample Code:
 // try {
@@ -19,10 +24,6 @@ echo "hello";
 //     die(print_r($e));
 // }
 
-// // SQL Server Extension Sample Code:
-// $connectionInfo = array("UID" => "booking-admin", "pwd" => "Anisham733@123", "Database" => "travel-booking-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-// $serverName = "tcp:travel-booking-server.database.windows.net,1433";
-// $conn = sqlsrv_connect($serverName, $connectionInfo);
 
 // if(isset($_POST['submit'])){
 //     echo "hello";
